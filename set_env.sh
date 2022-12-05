83a615043b3dc69ab60c6fc6265821ca82ed45ae
Tandev2k <superjunior242@gmail.com>
2022-12-05 21:08:33 +0700

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=tan240600
+export POSTGRES_HOST=db-images-filter.clmuy10iq2rl.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=images-filter-udacity
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=tandev2k-udacity
+export URL=http://localhost:8100
