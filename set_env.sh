4c4afd46075f1e7d5b20cc94847a51075574871d
Tandev2k <superjunior242@gmail.com>
2023-03-05 13:05:55 +0700

    update code

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=tan240600
-export POSTGRES_HOST=db-images-filter.clmuy10iq2rl.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=tandev2k-db.cuxlgg8sq1qo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=images-filter-udacity
+export AWS_BUCKET=tandev2k-s3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=tandev2k-udacity
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'adab153a56cb64579bfde6d956ea97a6-379686035.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
