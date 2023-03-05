7f56af31189986b087e8c2a2b83a64e0e8e33d52
Tandev2k <superjunior242@gmail.com>
2023-03-05 13:59:00 +0700

    fix host

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'a883018b5710d42f29b1276103fce207-1161143422.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://a883018b5710d42f29b1276103fce207-1161143422.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
